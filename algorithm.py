f75d00c1ebfbf26fdde72656d1e189aa4978910b
RaulNG-tec <a01652351@itesm.mx>
2020-08-30 03:25:16 -0500

    Window hecha con el canvas

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,10 @@
+from math import *
+
 class DataBase():
     #usuarios (owner/user)
-
     #funcion para añadir usuario
     #funcion para checar usuario
-    
 
-class User():
-    #reservacion, nombre, telefono 
-
-    #funcion para reservar
-    #funcion para consultar reserva
-    #funcion para cancelar reserva 
 
 class Reservation():
     #numero de personas, mesa asignada, hora {dure una hora}, numero de reserva
@@ -21,14 +15,146 @@ class Reservation():
     
 
 class Owner():
-    # restaurante, espacio establecido, maxima ocupacion, distancia entre personas, porcentaje de ocupacion, office hours
+    # restaurante, largo y ancho del local, trablecer espacio {maxima ocupacion, distancia, porcentaje}
+    def __init__(self,name,leng,wdt,tbl,perc,dis):
+        self.__name =   name
+        self.__leng =   leng
+        self.__wdt  =   wdt
+        self.__tbl  =   tbl
+        self.__perc =   perc
+        self.__dis  =   dis
 
-    # funcion para estrablecer espacio {maxima ocupacion, distancia, porcentaje}
-    # funcion para cambiar distancia 
+        spacejam=[]
+        for i in range (length*2):
+            line=[]
+            for j in range(width*2):
+                line.append(0)
+            spacejam.append(line)
+        
+        self.__stablishment=spacejam
+    
+    @dis.setter
+    def distance_setter(self, val):
+        self.__dis = val
+    
+    @perc.setter
+    def occupancy_percent(self, val):
+        self.__perc = val
+    
+    @property
+    def lenght(self):
+        return self.__leng
+    
+    @property
+    def width(self):
+        return self.__wdt
+    
+    @property
+    def maximum_tables(self):
+        return self.__tbl
+    
     # funcion para cambiar ocupacion 
+    @property
+    def maximum_occupancy(self):
+        return self.__perc
+    
+    # funcion para cambiar distancia 
+    @property
+    def safe_distance(self):
+        return self.__dis
+    
+    @property
+    def stablishment(self):
+        return self.__stablishment
+    
+    def make_reservation(persons):
+        
     # funcion para reservar asiento
     # funcion para liberar asiento 
     # funcion para ver lugar
 
-def spaceMaker(mx_ocup,distance,percent):
+
+class User():
+
+    def __init__(self,name,phone):
+        self.__name=name
+        self.__phone=phone
     
+    @name.setter
+    def name(self, tag):
+        self.__name = tag
+
+    @property
+    def name(self):
+        return self.__name
+
+    @phone.setter
+    def phone(self, value):
+        self.__phone = value
+
+    @property
+    def phone(self):
+        return self.__phone
+    
+    def do_reservation(self, persons):
+        
+
+    def cancel_reservation(self):
+        self.__reservation=NULL
+    #funcion para reservar
+    #funcion para consultar reserva
+    #funcion para cancelar reserva 
+
+
+
+
+
+class Stablishment():
+    # CAD en .SLD o .PRT 
+
+
+
+def space_maker(space,mx_ocup,distance,percent):
+    
+    tables =  math.floor((mx_ocup*percent)/100))
+    area= length * width
+    
+    if math.ciel(area/tables)<distance :
+        #mensaje de error
+    else :
+        ocupi(math.ciel(area/tables)*100)/area)
+    
+
+
+    return spacejam
+
+class Punto2D:
+    def __str__(self):
+        return f'Punto: x={self.x}, y={self.y} '
+
+    def __init__(self,x,y):
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    def distancia_euclidiana(self,punto2):
+        return((self.x-punto2.x)**2+(self.y-punto2.y)**2)**0.5
+
+    def punto_medio(self,punto2):
+        return Punto2D((self.x+punto2.x)/2,(self.y+punto2.y)/2)
+    
+    def __add__(self, p):
+        return Punto2D(self.x + p.x, self.y + p.y)
--- ejemplo.py
+++ ejemplo.py
@@ -1,41 +1,56 @@
 from tkinter import *
+from math import log, e
 
-window = Tk()
-# width, depth = (input("Width: "), input("Depth: "))
-# window.geometry(width+"x"+depth)
-SCREEN_WIDTH = window.winfo_screenwidth() #int
-SCREEN_HEIGHT = window.winfo_screenheight()
+COLORS = {'white': '#FFFFFF','light_blue':'#B6DDF2','bright_blue':'#6CC2F0','medium_blue':'5598BD','dark_blue':'#04507A','darkest_blue':'#325B70'}
 
-window.geometry(str(SCREEN_WIDTH//2)+"x"+str(SCREEN_HEIGHT//2))
-
-text_entry_width      = Entry(window, bg = '#8f9b9c') #font = "Helvetica 40"
-text_entry_depth      = Entry(window, bg = '#8f9b9c') #font = "Helvetica 40"
-text_entry_percentage = Entry(window, bg = '#8f9b9c')
-
-label_width = Label(window, text = text_entry_width.get(), bg = "blue")) #expand = True, fill = BOTH
-label_width = Label(window, text = text_entry_width.get(), bg = "blue"))
-
-canvas = Canvas()
-
-def funcEnter(text,text2):
+def funcEnter(text_entry_width,text_entry_depth,canvas,button_start,button_back):
+    canvas.create_rectangle(0, 0, int(text_entry_width.get()), int(text_entry_depth.get()), outline=COLORS['darkest_blue'], fill=COLORS['darkest_blue'])
+    canvas.pack()
     text_entry_width.pack_forget()
     text_entry_depth.pack_forget()
     button_start.pack_forget()
-    canvas.create_rectangle(30, 10, 120, 80, outline="#fb0", fill="#fb0")
-    canvas.pack()
     button_back.pack()
 
-def funcBack():
+def funcBack(text_entry_width, text_entry_depth, button_start, canvas, button_back):
     text_entry_width.pack()
     text_entry_depth.pack()
     button_start.pack()
     canvas.pack_forget()
     button_back.pack_forget()
 
-button_start = Button(window, text = "Enter", padx = 20, pady = 10, command = lambda: funcEnter(text_entry_width.get(),text_entry_depth.get()))
-button_back = Button(window,text = "<-", padx = 20, pady = 10, command = funcBack)
+def main():
+    window = Tk(className="Solución reto BlueYonder")
+    window.configure(bg = "black")
 
-text_entry_width.pack()
-text_entry_depth.pack()
-button_start.pack()
-window.mainloop()
+    SCREEN_WIDTH = window.winfo_screenwidth() #int
+    SCREEN_HEIGHT = window.winfo_screenheight()
+
+    window.geometry(str(SCREEN_WIDTH//2)+"x"+str(SCREEN_HEIGHT//2))
+
+    text_entry_width      = Entry(window, bd = 0, fg = COLORS['light_blue'], bg = COLORS['dark_blue']) #font = "Helvetica 40"
+    text_entry_depth      = Entry(window, bd = 0, fg = COLORS['light_blue'], bg = COLORS['dark_blue']) #font = "Helvetica 40"
+    text_entry_percentage = Entry(window, bd = 0, fg = COLORS['light_blue'], bg = COLORS['dark_blue'])
+
+    label_width      = Label(window, text = "Width", bg = COLORS['darkest_blue'], fg = 'white') #expand = True, fill = BOTH
+    label_depth      = Label(window, text = "Depth", bg = COLORS['darkest_blue'], fg = 'white')
+    label_percentage = Label(window, text = "Percentage", bg = COLORS['darkest_blue'], fg = 'white')
+
+    window.update()
+
+    canvas = Canvas(window, width = window.winfo_width()*3//4, height = window.winfo_height()*3//4)
+    
+    button_start = Button(window, text = "Enter", padx = 20, pady = 10, command = lambda: funcEnter(text_entry_width,text_entry_depth,canvas,button_start,button_back))
+    button_back = Button(window,text = "<-", padx = 20, pady = 10, command = lambda: funcBack(text_entry_width, text_entry_depth, button_start, canvas, button_back))
+
+    label_width.pack()
+    text_entry_width.pack()
+    label_depth.pack()
+    text_entry_depth.pack()
+    label_percentage.pack()
+    text_entry_percentage.pack()
+
+    button_start.pack()
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
