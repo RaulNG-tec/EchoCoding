dbc064d1b4d0994b00230c63c77ef165afb407b8
RaulNG-tec <a01652351@itesm.mx>
2020-08-29 22:25:40 -0500

    Creación de Carpeta tampletes

--- BlueYonder/pages/views.py
+++ BlueYonder/pages/views.py
@@ -2,5 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def Home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>") #string of HTML code
+def home_view(request,*args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    # return HttpResponse("<h1>Hello World</h1>") #string of HTML code
+    return render(request, "home.html", {})
+
+def contact_view(request,*args, **kwargs):
+    print(request.user)
+    return HttpResponse("<h1>Contact Page</h1>") #string of HTML code
+
